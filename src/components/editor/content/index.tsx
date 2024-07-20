65a7a976176a99de46a5482e9d26e7dae2d714ef
Muhamad Amir <m.amirazmi@hotmail.com>
2024-07-20 13:22:24 +0800

    Add environment variables

--- src/components/editor/content/index.tsx
+++ src/components/editor/content/index.tsx
@@ -4,7 +4,6 @@ import { useRef, useState } from "react";
 import { EditorContentProps } from "./types";
 import { IFrame } from "../iframe";
 import { useAppSelector } from "@/redux";
-import { cn } from "@/lib/utils";
 
 const minBreakpoints = {
   xs: "390px",
@@ -15,10 +14,15 @@ const minBreakpoints = {
 };
 
 export const EditorContent: React.FC<EditorContentProps> = () => {
-  const [src] = useState("http://localhost:3000/preview");
   const iframeRef = useRef(null);
 
   const view = useAppSelector((state) => state.editorResponsiveFrame.view);
 
-  return <IFrame src={src} width={minBreakpoints[view]} ref={iframeRef} />;
+  return (
+    <IFrame
+      src={process.env.NEXT_PUBLIC_PREVIEW_URL as string}
+      width={minBreakpoints[view]}
+      ref={iframeRef}
+    />
+  );
 };
